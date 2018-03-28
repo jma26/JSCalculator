ca8446d144d0986492a7f24d3cb0b24263793fe0
jma26 <j.masonoma17@gmail.com>
2018-03-28 00:04:54 -0700

    Operator and display functionality implemented

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 $(document).ready(function() {
     var value;
-    var total = 0;
+    var total = '';
+    var display = '';
     var operator;
+    var evaluation;
     // Default total is 0 on display
     $('#display').text(total);
     
     // All Clear function
     $('#ac').click(function() {
-        total = 0;
+        total = '';
+        display = '';
         $('#display').text(total);
+    })
+
+    // Clear Input function
+    $('#ce').click(function() {
+        display = '';
+        $('#display').text(display);
     })
 
     // Operator function
@@ -16,55 +25,69 @@ $(document).ready(function() {
 
         if ($(this).attr('id') == '/') {
             operator = '/';
-            total += operator;
+            total = eval(total) + operator;
             $('#display').text(total);
             console.log(total);
             console.log($(this).text() + 'was chosen');
         } else if ($(this).attr('id') == 'x') {
             operator = '*';
-            total += operator;
+            total = eval(total) + operator;
             $('#display').text(total);
             console.log(total);
             console.log($(this).text() + 'was chosen');
         } else if ($(this).attr('id') == 'minus') {
             operator = '-';
-            total += operator;
+            total = eval(total) + operator;
             $('#display').text(total);
             console.log(total);
             console.log($(this).text() + 'was chosen');
         } else if ($(this).attr('id') == 'plus') {
             operator = '+';
-            total += operator;
+            total = eval(total) + operator;
             $('#display').text(total);
             console.log(total);
             console.log($(this).text() + 'was chosen');
-        } else if ($(this).attr('id') == '%') {
-            operator = '%';
-            total += operator;
-            $('#display').text(total);
-            console.log(total);
-            console.log($(this).text() + 'was chosen');
-            // Evaluating expression
         } else if ($(this).attr('id') == '=') {
-            // Regex goes here, split expression into binary expression
+            // Evaluate
+            total = eval(total)
             $('#display').text(total);
             console.log(total);
             console.log($(this).text() + 'was chosen');
-            
         }
-
-
-
     })
 
     $('.number').click(function() {
-        value = $(this).text();
-        if (total === 0) {
-            total = value;
-            $('#display').text(total);
+
+        if (typeof total === 'number') {
+
+            // Concatenate the decimal
+            if ($(this).attr('id') === '.') {
+                total += '.';
+                console.log(total);
+                $('#display').text(total);
+            } else {
+                value = parseInt($(this).text());
+                // Parse the total from Number to a String for concatenation
+                // String + Number enables easy concatenation '3' + 5 = '35'
+                total = String(total);
+                total += value;
+                $('#display').text(total);
+            }
+
         } else {
-            total += value;
-            $('#display').text(total);
+            
+            // Concatenate the decimal if total is a string
+            if ($(this).attr('id') === '.') {
+                value = '.';
+                total += value;
+                $('#display').text(total);
+                console.log(total);
+            } else {
+                value = parseInt($(this).text());
+                total += value;
+                $('#display').text(total);
+                console.log(total);
+            }
         }
     })
 })
